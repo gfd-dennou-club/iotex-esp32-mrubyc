d5ffa2ae7f3ee463f3533d72c6e790243c9a0a58
USER <user@es>
2020-10-12 00:05:03 +0900

    test for students

--- mrblib/models/aqm0802a.rb
+++ mrblib/models/aqm0802a.rb
@@ -8,34 +8,27 @@ class AQM0802A
     @i2c = i2c
   end
 
-  def lcd_write(opcode, data)
-    n = 0
-    while n < data.length
-      @i2c.write(0x3e, [opcode, data[n]])
-      n += 1
-    end
-  end
-
   def setup
-    lcd_write(0x00, [0x38, 0x39, 0x14, 0x70, 0x56, 0x6c])
+    @i2c.write(0x3e, [0x00, 0x38, 0x39, 0x14, 0x70, 0x56, 0x6c])
     sleep(1)
-    lcd_write(0x00, [0x38, 0x0c, 0x01])
+    @i2c.write(0x3e, [0x00, 0x38, 0x0c, 0x01])
   end
 
   def clear
-    lcd_write(0x00, [0x01])
+    @i2c.write(0x3e, [0x00, 0x01])
   end
 
   def cursor(x, y)
-    lcd_write(0x00, [0x80 + (0x40 * y + x)])
+    @i2c.write(0x3e, [0x00, 0x80 + (0x40 * y + x)])
   end
 
   def write_string(s)
     a = Array.new
+    a.push(0x40)
     s.length.times do |n|
       a.push(s[n].ord)
     end
-    lcd_write(0x40, a)
+    @i2c.write(0x3e, a)
   end
 
 end
